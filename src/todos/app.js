d47e1db43bb80e6d2d0dd1703389f68228969464
Alberto-T <alberto.torres.cancino@gmail.com>
2024-03-19 19:27:12 -0300

    first commit

---
+++ src/todos/app.js
@@ -0,0 +1,15 @@
+import html from "./app.html?raw";
+/**
+ * 
+ * @param {String} elementId 
+ */
+
+export const App = (elementId) =>{
+    
+    // Función anónima autoinvocada ; Cuando la función App() se llama
+    (()=> {
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementId).append(app);
+    })();
+}
